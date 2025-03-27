4033b50f9cfb78fc66e229be533f61092d1de53c
Szymon Samus <siizmonsus@gmail.com>
2025-03-27 19:42:02 +0100

    FIX: PATHING ISSUES

--- src/pages/PartyMaker.js
+++ src/pages/PartyMaker.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Typewriter from '../components/';
+import { Typewriter } from '../components';
 const moment = require('moment');
 
 const PartyMaker = ({ config, eventUserData, latestEvent, formatDateTime }) => {
