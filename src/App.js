9baf26f83745c9de7220c97b83c5f790e49c245c
Szymon Samus <siizmonsus@gmail.com>
2025-04-04 21:20:46 +0200

    FIX: App rendering before auth is set, which causes token loss.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,34 @@ import AppContent from './AppContent';
 
 function App() {
   const [auth, setAuth] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const storedAuth = localStorage.getItem('auth')
     if (storedAuth) {
       setAuth(JSON.parse(storedAuth));
     }
+    setIsLoading(false);
   }, []);
+
+  if (isLoading) {
+    return <div className="flex justify-center items-center h-screen bg-zinc-900 text-white">Loading...</div>;
+  }
 
   return (
     <Router>
       <Routes>
-        <Route path="/bot-dashboard/login" element={<Login setAuth={setAuth} />} />
-        <Route path="/bot-dashboard" element={auth ? <Navigate to={`/bot-dasboard/${auth.guildId}`} /> : <Navigate to="/bot-dashboard/login" />} />
-        <Route path="/bot-dashboard/:guildId/*" element={auth ? <AppContent auth={auth} setAuth={setAuth} /> : <Navigate to="/bot-dashboard/login" />} />
+        <Route 
+          path="/bot-dashboard/login"
+          element={<Login setAuth={setAuth} />} />
+        <Route 
+          path="/bot-dashboard" 
+          element={auth ? <Navigate to={`/bot-dasboard/${auth.guildId}`} /> : 
+          <Navigate to="/bot-dashboard/login" />} />
+        <Route 
+          path="/bot-dashboard/:guildId/*" 
+          element={auth ? <AppContent auth={auth} setAuth={setAuth} /> : 
+          <Navigate to="/bot-dashboard/login" />} />
       </Routes>
     </Router>
   );
